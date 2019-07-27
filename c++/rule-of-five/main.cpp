0104b5b64d00b94cb584d9899b92c1df16454f17
bananaappletw <bananaappletw@gmail.com>
2019-07-27 16:23:00 +0800

    Update rule of five

--- c++/rule-of-five/main.cpp
+++ c++/rule-of-five/main.cpp
@@ -1,6 +1,30 @@
-#include "main.h"
+#include "person.hpp"
+
 int main()
 {
-    Person bill = Person("Bill", "My friend");
-    Person john = Person(bill);
+    // constructor
+    Person empty_person = Person();
+
+    // custom construtor
+    Person custom_person = Person("Bill", "My friend");
+
+    // copy construtor
+    Person copy_person = Person(custom_person);
+
+    // move constructor
+    Person move_person = Person(std::move(custom_person));
+
+    // copy assignment
+    // Person assignment_person = move_person;
+    // copy constructor called and move construtor to initialize instead
+    Person assignment_person = Person();
+    assignment_person = move_person;
+
+    // move assignment
+    // Person move_assignment_person = std::move(move_person);
+    // move constructor called to initialize instead
+    Person move_assignment_person = Person();
+    move_assignment_person = std::move(move_person);
+
+
 }
